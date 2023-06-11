9dbc9657ba1c40b9c0b30e606bf5eedb244b91b2
Oliver Caldwell <oliver.caldwell@viasat.com>
2023-06-11 19:46:54 +0100

    Set vim as a global in lua and fennel, hope this didn't make anything wonky

--- plugins/mason.fnl
+++ plugins/mason.fnl
@@ -1,20 +1,22 @@
 (local uu (require :user.util))
+(local utils (uu.autoload :astronvim.utils))
+(local lspconfig (uu.autoload :lspconfig))
 
 [(uu.tx
    :williamboman/mason-lspconfig.nvim
    {:opts (fn [_ opts]
-            (set opts.ensure_installed
-                 ((. (require :astronvim.utils) :list_insert_unique) opts.ensure_installed
-                  [:fennel_language_server])))})
+            (set opts.ensure_installed (utils.list_insert_unique opts.ensure_installed [:fennel_language_server]))
+            (set opts.handlers.lua_ls
+                 (fn []
+                   (lspconfig.lua_ls.setup
+                     {:settings {:Lua {:diagnostics {:globals [:vim]}}}})
+                   (lspconfig.fennel_language_server.setup
+                     {:settings {:fennel {:diagnostics {:globals [:vim]}}}}))))})
  (uu.tx
    :jay-babu/mason-null-ls.nvim
    {:opts (fn [_ opts]
-            (set opts.ensure_installed
-                 ((. (require :astronvim.utils) :list_insert_unique) opts.ensure_installed
-                  {})))})
+            (set opts.ensure_installed (utils.list_insert_unique opts.ensure_installed [])))})
  (uu.tx
    :jay-babu/mason-nvim-dap.nvim
    {:opts (fn [_ opts]
-            (set opts.ensure_installed
-                 ((. (require :astronvim.utils) :list_insert_unique) opts.ensure_installed
-                  {})))})]
+            (set opts.ensure_installed (utils.list_insert_unique opts.ensure_installed)))})]
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,14 +1,21 @@
 local uu = require("user.util")
+local utils = uu.autoload("astronvim.utils")
+local lspconfig = uu.autoload("lspconfig")
 local function _1_(_, opts)
-  opts.ensure_installed = (require("astronvim.utils")).list_insert_unique(opts.ensure_installed, {"fennel_language_server"})
-  return nil
-end
-local function _2_(_, opts)
-  opts.ensure_installed = (require("astronvim.utils")).list_insert_unique(opts.ensure_installed, {})
+  opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {"fennel_language_server"})
+  local function _2_()
+    lspconfig.lua_ls.setup({settings = {Lua = {diagnostics = {globals = {"vim"}}}}})
+    return lspconfig.fennel_language_server.setup({settings = {fennel = {diagnostics = {globals = {"vim"}}}}})
+  end
+  opts.handlers.lua_ls = _2_
   return nil
 end
 local function _3_(_, opts)
-  opts.ensure_installed = (require("astronvim.utils")).list_insert_unique(opts.ensure_installed, {})
+  opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {})
   return nil
 end
-return {uu.tx("williamboman/mason-lspconfig.nvim", {opts = _1_}), uu.tx("jay-babu/mason-null-ls.nvim", {opts = _2_}), uu.tx("jay-babu/mason-nvim-dap.nvim", {opts = _3_})}
+local function _4_(_, opts)
+  opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed)
+  return nil
+end
+return {uu.tx("williamboman/mason-lspconfig.nvim", {opts = _1_}), uu.tx("jay-babu/mason-null-ls.nvim", {opts = _3_}), uu.tx("jay-babu/mason-nvim-dap.nvim", {opts = _4_})}
